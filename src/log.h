a80edb5b6664a02560167a2fdd19b2615807b4cd
down <Tian_erjie1316@163.com>
2024-06-15 21:19:49 +0800

    为LogAppender派生出两个实例-->控制台输出和文件输出

--- src/log.h
+++ src/log.h
@@ -77,4 +77,94 @@ namespace sylar_w
         std::vector<FormatItem::ptr> items_;
         bool error_ = false;
     };
+
+    // 日志输出地
+    class LoggerAppender
+    {
+    public:
+        using ptr = std::shared_ptr<LoggerAppender>;
+
+    public:
+        LoggerAppender() = default;
+        virtual ~LoggerAppender();
+        virtual void doLog(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
+        LogFormatter::ptr getFormat() const { return format_; };
+        void setFormat(LogFormatter::ptr format) { format_ = format_; }
+        /**
+         * @brief 获取日志级别
+         */
+        LogLevel::Level getLevel() const { return level_; }
+
+        /**
+         * @brief 设置日志级别
+         */
+        void setLevel(LogLevel::Level val) { level_ = val; }
+
+    protected:
+        LogLevel::Level level_;
+        LogFormatter::ptr format_;
+    };
+    // 日志器
+    class Logger
+    {
+    public:
+        using ptr = std::shared_ptr<Logger>;
+
+    public:
+        Logger(const std::string &name = "root");
+
+        /* 调用此函数记录日志：
+        参数：
+            level :要记录的日志级别。
+            event: 要记录的日志事件。
+             */
+        void log(LogLevel::Level level, LogEvent::ptr event);
+        void debug(LogEvent::ptr event);
+        void info(LogEvent::ptr event);
+        void warn(LogEvent::ptr event);
+        void error(LogEvent::ptr event);
+        void fatal(LogEvent::ptr event);
+
+        void addAppender(LoggerAppender::ptr appender);
+        void delAppender(LoggerAppender::ptr appender);
+        LogLevel::Level getLevel() const
+        {
+            return level_;
+        }
+        void setLevel(LogLevel::Level val)
+        {
+            level_ = val;
+        }
+
+    private:
+        std::string name_;                         // 日志名称
+        LogLevel::Level level_;                    // 日志级别
+        std::list<LoggerAppender::ptr> appenders_; // Appender集合
+    };
+
+    // 输出到控制台
+    class StdoutLogAppender : public LoggerAppender
+    {
+    public:
+        using ptr = std::shared_ptr<StdoutLogAppender>;
+
+    public:
+        void doLog(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) override;
+    };
+
+    // 输出到文件
+    class FileLogAppender : public LoggerAppender
+    {
+    public:
+        using ptr = std::shared_ptr<FileLogAppender>;
+
+    public:
+        FileLogAppender(const std::string &filename);
+        void doLog(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) override;
+        bool reopen();
+
+    private:
+        std::string filename_;
+        std::ofstream filestream_;
+    };
 } // namespace sylar_w
